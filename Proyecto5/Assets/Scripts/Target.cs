54b51be5362d1ab22c9c7d84b957334c5b4eb3b2
JuanjoAlcover11 <juanjoalcover2001@gmail.com>
2022-02-16 13:24:34 +0100

    2

---
+++ Proyecto5/Assets/Scripts/Target.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    private float lifeTime = 2f;
+    private GameManager gameManagerScript;
+    void Start()
+    {
+        Destroy(gameObject, lifeTime);
+        gameManagerScript = FindObjectOfType<GameManager>();
+    }
+
+    private void OnMouseDown()
+    {
+        Destroy(gameObject);
+
+        if (gameObject.CompareTag("Bad"))
+        {
+            gameManagerScript.isGameOver = true;
+        }
+    }
+
+    private void OnDestroy()
+    {
+        gameManagerScript.targetPositions.Remove(transform.position);
+    }
+}
